b055422488b1ba3416a41f730cc359d14a7f038b
zane <83908895+grsszne@users.noreply.github.com>
2025-11-06 13:22:07 -0600

    Update items.js

--- src/app/components/items.js
+++ src/app/components/items.js
@@ -54,6 +54,7 @@ items = [
             },
         ],
     },
+    /*
     {
         title: "Driving a Manual Car in 2025",
         description: "My experience learning to drive a manual car",
@@ -86,6 +87,7 @@ items = [
             },
         ],
     },
+    */
     {
         title: "Ejecting Rover 2024 - 2025",
         description: "Progression and repots from my 2024 - 2025 NASA HUNCH project",
